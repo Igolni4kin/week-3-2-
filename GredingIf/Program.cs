0e57db6ee2b57df7543d4325ce58ca8ce0ebb9b6
Igolni4kin <101661832+Igolni4kin@users.noreply.github.com>
2022-04-08 09:37:14 +0300

    Add files via upload

---
+++ GredingIf/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GredingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // приложение просит пользователя ввести результат его экзаменв
+            //результат экзамена это одна буква от A до F
+            //A - Oustanding
+            //B - Superior
+            //C - Good
+            //D - Satisfactory
+            //E - Low Pass
+            //F - Failure
+            // в работе приложения испльзуется if-statements
+            Console.WriteLine("Enter your exam results");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if(userInput == 'A')
+            {
+                Console.WriteLine("Outstanding");
+            }
+            else if (userInput == 'B')
+            {
+                Console.WriteLine(" Superior");
+            }
+            else if (userInput == 'C')
+            {
+                Console.WriteLine(" Good");
+            }
+            else if (userInput == 'D')
+            {
+                Console.WriteLine(" Satisfactory");
+            }
+            else if (userInput == 'E')
+            {
+                Console.WriteLine("Low Pass");
+            }
+            else if (userInput == 'F')
+            {
+                Console.WriteLine("Failure");
+            }
+
+
+
+        }
+    }
+}
---
+++ GredingSwitch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GredingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // приложение просит пользователя ввести результат его экзаменв
+            //результат экзамена это одна буква от A до F
+            //A - Oustanding
+            //B - Superior
+            //C - Good
+            //D - Satisfactory
+            //E - Low Pass
+            //F - Failure
+            // в работе приложения испльзуется switch()
+            Console.WriteLine("Enter your exam results");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch(userInput)
+            {
+                case 'A':
+                Console.WriteLine("Outstanding");
+                    break;
+                case 'B':
+                    Console.WriteLine("Superior");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 'E':
+                    Console.WriteLine("Low Pass");
+                    break;
+                case 'F':
+                    Console.WriteLine("Failure");
+                    break;
+
+
+            }
+        }
+    }
+}
